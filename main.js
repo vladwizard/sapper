263da96951ae8275df8d9df9ad2f4d7faccd6fec
vladwizard <v290801@gmail.com>
2023-03-04 05:41:26 +0300

    Update main.js

--- main.js
+++ main.js
@@ -117,10 +117,33 @@ class Cell {
       }
     }
   }
+  LeftClick(counter) {
+    if (this.hiden) {
+      if (this.flaged) {
+        counter.SetNumber(counter.number + 1);
+        this.flaged = false;
+        this.quetion = true;
+        this.element.className = "quetionHide";
+      } else if (this.quetion) {
+        this.flaged = false;
+        this.quetion = false;
+        this.element.className = "hiden";
+      } else {
+        this.flaged = true;
+        this.element.className = "flaged";
+        counter.SetNumber(counter.number - 1);
+      }
+    }
+  }
 }
 
 class Sapper {
-  paths = [
+  timer = new Timer(document.getElementById("timer"));
+  minesCounter = new MinesCounter(document.getElementById("minesCounter"));
+  smiley = document.getElementById("smiley");
+  area = document.querySelector("main");
+  
+  pathsAround = [
     { i: -1, j: -1 },
     { i: -1, j: 0 },
     { i: -1, j: 1 },
@@ -132,110 +155,121 @@ class Sapper {
   ];
   size = 16;
   startMines = 40;
+  cellsMat = [];
 
-  mat = [];
-  timer = new Timer(document.getElementById("timer"));
-  minesCounter = new MinesCounter(document.getElementById("minesCounter"));
-  smiley = document.getElementById("smiley");
-  area = document.querySelector("main");
   constructor() {
-    this.timer.Start();
-    this.minesCounter.SetNumber(this.startMines);
+    this.cellCount = this.size * this.size;
+    this.smiley.onclick = () => this.ResetCells();
     this.BuildArea();
-    this.smiley.onclick = () => this.Reset();
-    this.unopenedCell = this.size * this.size - this.startMines;
+    this.SetInitialValues();
   }
-  Reset() {
-    this.unopenedCell = this.size * this.size - this.startMines;
+  BuildArea() {
+    for (let i = 0; i < this.size; i++) {
+      let newArr = [];
+      for (let j = 0; j < this.size; j++) {
+        let newCell = this.CreateCell(document.createElement("div"), i, j);
+        this.area.append(newCell.element);
+        newArr.push(newCell);
+      }
+      this.cellsMat.push(newArr);
+    }
+  }
+  CreateCell(element, i, j) {
+    return new Cell(
+      element,
+      () => {
+        this.Click(i, j);
+      },
+      this.minesCounter,
+      this.smiley
+    );
+  }
+  SetInitialValues() {
     this.timer.Start();
+    this.unopenedCell = this.cellCount - this.startMines;
     this.minesCounter.SetNumber(this.startMines);
     this.firstClick = true;
+  }
+  ResetCells() {
+    this.SetInitialValues();
     this.smiley.className = "";
     for (let i = 0; i < this.size; i++) {
       for (let j = 0; j < this.size; j++) {
-        this.mat[i][j] = new Cell(
-          this.mat[i][j].element,
-          () => {
-            this.Click(i, j);
-          },
-          this.minesCounter,
-          this.smiley
+        this.cellsMat[i][j] = this.CreateCell(
+          this.cellsMat[i][j].element,
+          i,
+          j
         );
       }
     }
   }
-  firstClick = true;
   Click(i, j) {
     if (this.firstClick) {
       this.firstClick = false;
       this.FillByMines({ i, j });
-      this.CountMinesInAllCells();
-      this.unopenedCell--;
+      this.CountMinesAroundInAllCells();
     }
-    let cell = this.mat[i][j];
+    let cell = this.cellsMat[i][j];
     if (cell.mined) {
       cell.Show();
       cell.element.className = "explosion";
       this.Lose();
     } else if (cell.minesAround == 0) {
-      function aroundHandle(i, j, mat, paths, reduce, further = true) {
-        mat[i][j].Show();
+      function aroundHandle(i, j, cellsMat, pathsAround, reduce, further = true) {
+        cellsMat[i][j].Show();
         reduce();
         if (further) {
-          for (let path of paths) {
-            let row = mat[i + path.i];
-            if (row) {
-              let current = row[j + path.j];
-              if (current) {
-                if (current.hiden == true) {
-                  if (current.minesAround == 0) {
-                    aroundHandle(
-                      i + path.i,
-                      j + path.j,
-                      mat,
-                      paths,
-                      reduce,
-                      true
-                    );
-                  } else {
-                    aroundHandle(
-                      i + path.i,
-                      j + path.j,
-                      mat,
-                      paths,
-                      reduce,
-                      false
-                    );
-                  }
+          for (let path of pathsAround) {
+            let current = cellsMat[i + path.i]?.[j + path.j];
+            if (current) {
+              if (current.hiden == true) {
+                if (current.minesAround == 0) {
+                  aroundHandle(
+                    i + path.i,
+                    j + path.j,
+                    cellsMat,
+                    pathsAround,
+                    reduce,
+                    true
+                  );
+                } else {
+                  aroundHandle(
+                    i + path.i,
+                    j + path.j,
+                    cellsMat,
+                    pathsAround,
+                    reduce,
+                    false
+                  );
                 }
               }
             }
           }
         }
       }
-      aroundHandle(i, j, this.mat, this.paths, () => this.unopenedCell--);
+      let byMe = aroundHandle(i, j, this.cellsMat, this.pathsAround, () => this.unopenedCell--);
     } else {
       cell.Show();
       this.unopenedCell--;
     }
-    if (this.unopenedCell == -1) this.Win();
+    if (this.unopenedCell == 0) this.Win();
   }
 
-  Show() {
-    for (let i = 0; i < this.size; i++) {
-      for (let j = 0; j < this.size; j++) {
-        this.mat[i][j].Show();
-      }
-    }
-  }
   Win() {
     this.smiley.className = "cool";
     this.timer.Stop();
-  }
-  Lose() {
     for (let i = 0; i < this.size; i++) {
       for (let j = 0; j < this.size; j++) {
-        let cell = this.mat[i][j];
+        cell.Stop();
+      }
+    }
+  }
+  Lose() {
+    this.smiley.className = "sad";
+    this.timer.Stop();
+    for (let i = 0; i < this.size; i++) {
+      for (let j = 0; j < this.size; j++) {
+        let cell = this.cellsMat[i][j];
         if (cell.hiden) {
           if (cell.flaged && !cell.mined) cell.element.className = "mistake";
           else if (cell.mined) cell.element.className = "mined";
@@ -243,34 +277,15 @@ class Sapper {
         cell.Stop();
       }
     }
-    this.timer.Stop();
-    this.smiley.className = "sad";
   }
-  BuildArea() {
-    for (let i = 0; i < this.size; i++) {
-      let newArr = [];
-      for (let j = 0; j < this.size; j++) {
-        let newCell = new Cell(
-          document.createElement("div"),
-          () => {
-            this.Click(i, j);
-          },
-          this.minesCounter,
-          this.smiley
-        );
-        this.area.append(newCell.element);
-        newArr.push(newCell);
-      }
-      this.mat.push(newArr);
-    }
-  }
+
   FillByMines(except) {
-    let r = Array.from(Array(this.size * this.size).keys());
+    let r = Array.from(Array(this.cellCount).keys());
     r.splice(except.i * this.size + except.j, 1);
     for (let i = 0; i < this.startMines; i++) {
       let index = r.splice(randomInteger(0, r.length - 1), 1);
       let ost = index % this.size;
-      this.mat[(index - ost) / this.size][ost].Mine();
+      this.cellsMat[(index - ost) / this.size][ost].Mine();
     }
     function randomInteger(min, max) {
       let rand = min - 0.5 + Math.random() * (max - min + 1);
@@ -278,18 +293,17 @@ class Sapper {
     }
   }
 
-  CountMinesInAllCells() {
+  CountMinesAroundInAllCells() {
     for (let i = 0; i < this.size; i++) {
       for (let j = 0; j < this.size; j++) {
         let number = 0;
-        for (let path of this.paths) {
-          let row = this.mat[i + path.i];
-          if (row) if (row[j + path.j]?.mined) number++;
+        for (let path of this.pathsAround) {
+          if (this.cellsMat[i + path.i]?.[j + path.j]?.mined) number++;
         }
-        this.mat[i][j].minesAround = number;
+        this.cellsMat[i][j].minesAround = number;
       }
     }
   }
 }
 
-let sapper = new Sapper();
+new Sapper();
